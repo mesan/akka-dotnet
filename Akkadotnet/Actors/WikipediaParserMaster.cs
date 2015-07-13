aba9f2d94de79793a44d375bc820656d1eb67e93
Martin Harbu Bielecki <martinb@mesan.no>
2015-07-13 22:15:38 +0200

    Added limit for number of URLs parsed

--- Akkadotnet/Actors/WikipediaParserMaster.cs
+++ Akkadotnet/Actors/WikipediaParserMaster.cs
@@ -7,6 +7,8 @@ namespace Akkadotnet.Actors
     public class WikipediaParserMaster : ReceiveActor
     {
         private readonly HashSet<string> _visitedUrls;
+        private const int MaxSites = 100;
+
         public WikipediaParserMaster()
         {
             _visitedUrls = new HashSet<string>();
@@ -15,6 +17,11 @@ namespace Akkadotnet.Actors
 
         private void ParseNewUrl(string url)
         {
+            if (_visitedUrls.Count >= MaxSites)
+            {
+                //noe spennende som skal skje her? Drepe actoren?
+                return;
+            }
             if (!_visitedUrls.Contains(url))
             {
                _visitedUrls.Add(url);
